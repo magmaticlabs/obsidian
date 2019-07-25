f7d7b4227090abb011d99ab98a7faf93d29a2463
Matt Fields <mafields@0x6d617474.net>
2019-07-25 13:37:02 -0400

    Extract package headers into JSON document during archive

--- app/Scripts/buildscripts/archive.sh
+++ app/Scripts/buildscripts/archive.sh
@@ -14,6 +14,16 @@ truncate -s-2 /archive/checksums.json
 echo -e "\n}" >> /archive/checksums.json;
 echo "Done!"
 
+echo -n "Extracting package header data... "
+_header_file=$(grep -lZsiE "(Plugin|Theme) Name:" ./* | xargs -0 grep -lsi "Version:" | head -n 1)
+if [[ -z "${_header_file}" ]]; then
+    echo "Unable to locate file containing package header!"
+    exit 1
+fi
+# According to WordPress, the header must be in the first 8192 bytes of the file
+echo $(head -c 8192 ${_header_file} | php /scripts/header_extract.php) > /archive/header.json
+echo "Done!"
+
 echo -n "Rebasing files under new directory... "
 mkdir /staging/${SLUG}
 find /staging/* -maxdepth 0 ! -name "${SLUG}" -exec mv {} /staging/${SLUG} \;
---
+++ app/Scripts/buildscripts/header_extract.php
@@ -0,0 +1,26 @@
+<?php
+
+$file_data = str_replace("\r", "\n", file_get_contents('php://stdin'));
+
+$headers = [
+    'Name' => '(Plugin|Theme) Name',
+    'PackageURI' => '(Plugin|Theme) URI',
+    'Version' => 'Version',
+    'Description' => 'Description',
+    'Author' => 'Author',
+    'AuthorURI' => 'Author URI',
+    'TextDomain' => 'Text Domain',
+    'DomainPath' => 'Domain Path',
+    'Network' => 'Network',
+];
+
+foreach ($headers as $header => $regex) {
+    $pattern = '/^[ \t\/*#@]*' . $regex . ':(.*)$/mi';
+    if (preg_match($pattern, $file_data, $match) && !empty($match[1])) {
+        $headers[$header] = trim(preg_replace('/\s*(?:\*\/|\?>).*/', '', $match[count($match) - 1]));
+    } else {
+        $headers[$header] = '';
+    }
+}
+
+echo json_encode($headers);
